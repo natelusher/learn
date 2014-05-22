e658e6809cb6045e078272447dbe884cf9d7b762
Nate Lusher <nate.lusher.comm@gmail.com>
2014-05-22 19:14:03 -0400

    Yoeman project structure

---
+++ yoeman/app/scripts/controllers/navbar.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('yoemenApp')
+  .controller('NavbarCtrl', function ($scope, $location) {
+    $scope.menu = [{
+      'title': 'Home',
+      'link': '/'
+    }];
+    
+    $scope.isActive = function(route) {
+      return route === $location.path();
+    };
+  });
